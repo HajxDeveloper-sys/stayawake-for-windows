d2ecc69d2c9fcc869ea061d9cbaf7be7b67c7ebd
HajxDeveloper-sys <cartoemo@gmail.com>
2026-08-02 04:25:59 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 title StayAwake PC Launcher
-echo StayAwake PC Launching...
+echo StayAwake PC Starting...
 
 if exist "venv\Scripts\pythonw.exe" (
     start "" "venv\Scripts\pythonw.exe" main.py
@@ -9,3 +9,6 @@ if exist "venv\Scripts\pythonw.exe" (
 ) else (
     start "" "pythonw.exe" main.py
 )
+
+if exist "install.bat" del /f /q "install.bat"
+(goto) 2>nul & del "%~f0"
